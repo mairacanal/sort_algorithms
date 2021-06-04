09f2658e4fbcd24c3d7c66a4e0a10f3d8fafeb3f
mairacanal <maira.canal@usp.br>
2021-06-04 10:48:45 -0300

    Adding analysis to main.c

--- main.c
+++ main.c
@@ -1,43 +1,92 @@
+/*
+ * @file main.c
+ * @authors Guilherme Mafra (Nº USP: 11272015) and Maíra Canal (Nº USP: 11819403)
+ */
+
 #include <time.h>
 #include <math.h>
 #include "list.h"
 
 #define REP 10
 
+void random_numbers(list *l, long size, long max);
+void growing_numbers(list *l, long size, long max);
+void declining_numbers(list *l, long size, long max);
+void analysis(void (*sort)(list *), void (*numbers)(list *, long, long));
+
 int main(void) {
     
+    printf("ORDEM ALEATÓRIA\n");
+    analysis(quick_sort, random_numbers);
+
+    printf("ORDEM CRESCENTE\n");
+    analysis(quick_sort, growing_numbers);
+
+    printf("ORDEM DECRESCENTE\n");
+    analysis(quick_sort, declining_numbers);
+
+    return 0;
+
+}
+
+void random_numbers(list *l, long size, long max) {
+
+    for(int i = 0; i < size; i++)
+        insert_list(l, rand() % max);
+
+}
+
+void growing_numbers(list *l, long size, long max) {
+
+    random_numbers(l, size, max);
+    quick_sort(l);
+
+}
+
+void declining_numbers(list *l, long size, long max) {
+
+    list aux;
+
+    random_numbers(l, size, max);
+    quick_sort(l);
+
+    create_list(&aux);
+
+    for(int i = 0; i < size; i++)
+        insert_list(&aux, l->elements[size - i]);
+
+    for(int i = 0; i < size; i++)
+        l->elements[i] = aux.elements[i];
+
+    destroy_list(&aux);
+
+}
+
+void analysis(void (*sort)(list *), void (*numbers)(list *, long, long)) {
+
     list l;
     clock_t time_start;
-    clock_t times_add;
+    clock_t time_sum;
     
-    for (int ord = 2; pow(10, ord) <= TAM; ord++) {
+    for (int ord = 3; pow(10, ord) <= TAM; ord++) {
 
-        times_add = 0;
+        time_sum = 0;
 
         for (int i = 0; i < REP; i++) {
 
             create_list(&l);
-            
-            for (long j = 0; j < pow(10, ord); j++) {
-                insert_list(&l, rand() % TAM);
-            }
-
-            //print_list(&l);
+            numbers(&l, pow(10, ord), TAM);
 
             time_start = clock();
-            quick_sort(&l);
-            times_add += clock() - time_start;
-
-            //print_list(&l);
+            sort(&l);
+            time_sum += clock() - time_start;
 
             destroy_list(&l);
         
         }
 
-        printf("Tamanho: %ld Tempo total: %.10f\n", (long) pow(10, ord), (times_add / (float) REP)/CLOCKS_PER_SEC);
+        printf("Tamanho: %ld Tempo total: %.10f\n", (long) pow(10, ord), (time_sum / (float) REP)/CLOCKS_PER_SEC);
 
     }
-
-    return 0;
 
 }
