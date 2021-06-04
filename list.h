2c0d3fb11600866f72bad54b9050dbdbda7cc733
mairacanal <maira.canal@usp.br>
2021-06-04 08:37:40 -0300

    Adding all sort functions

--- list.h
+++ list.h
@@ -18,3 +18,7 @@ void create_list(list* l);
 void destroy_list(list* l);
 int insert_list(list* l, elem e);
 void print_list(list* l);
+void bubble_sort(list* l);
+void quick_sort(list* l);
+void radix_sort(list* l);
+void heapsort(list* l);
